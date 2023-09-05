5451cb6cfd20bffe3deb8af2266dcd6d07ac4762
luchungi <lu.chung.i@gmail.com>
2023-09-05 14:17:38 +0800

    Torch dataset py, trainer py, basic MLP model

---
+++ model/data.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+def create_df_from_yf(spx_df, vix_df, start_date, end_date, predict_t_plus_1=False):
+    '''
+    Create dataset format used for model defined above from yfinance dataframe
+    params:
+    spx_df: pandas.DataFrame
+        dataframe containing the S&P500 data from yfinance with datetime index
+    vix_df: pandas.DataFrame
+        dataframe containing the VIX data from yfinance with datetime index
+    start_date: datetime
+        start date of the dataset (included)
+    end_date: datetime
+        end date of the dataset (excluded)
+    predict_t_plus_1: bool
+        if True, the model will predict the VIX level at t+1, if False, the model will predict the VIX level at t
+    '''
+    # check that both dataframe have a datetime index
+    if not isinstance(spx_df.index, pd.DatetimeIndex):
+        raise ValueError('spx_df must have a datetime index')
+    if not isinstance(vix_df.index, pd.DatetimeIndex):
+        raise ValueError('vix_df must have a datetime index')
+
+    # calculate simple return and squared return of S&P500
+    spx = pd.DataFrame(columns=['r1', 'r2'])
+    spx['r1'] = spx_df.loc[start_date:end_date-pd.Timedelta(days=1), 'Close'].pct_change()
+    spx['r2'] = spx['r1'] ** 2
+
+    # extract VIX level
+    vix = vix_df.loc[start_date:end_date-pd.Timedelta(days=1), ['Close']] / 100
+    vix.columns = ['vix']
+
+    # check that both dataframes have same index
+    if not spx.index.equals(vix.index):
+        raise ValueError('spx_df and vix_df must have same index from start_date to end_date')
+
+    # shift VIX level if predict_t_plus_1 is True i.e. predict VIX level at t+1
+    if predict_t_plus_1:
+        vix = vix.shift(-1)
+
+    combined = pd.concat([spx, vix], axis=1)
+    combined = combined.dropna()
+
+    return combined
+
+class VIXDataset(Dataset):
+    '''
+    Dataset class for VIX data
+    x = (r1_sliding_window, r2_sliding_window, t) each with shape (len(df) - n + 1, n)
+    y = vix level at last time step of x
+    '''
+    def __init__(self, df, n, dtype=torch.float32):
+        '''
+        params:
+        df: pandas.DataFrame
+            dataframe containing the data with columns 'r1', 'r2', 'vix' where 'r1' is the simple return, 'r2' is the squared return and 'vix' is the VIX level to be predicted
+        n: int
+            sliding window size to calculate R1 and R2
+        '''
+        self.len = len(df) - n + 1
+         # create sliding window of size n for r1 and r2 with shape (len(df) - n + 1, n)
+        r1_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 0].values, n)
+        r2_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 1].values, n)
+
+        # calculate time difference between prediction datetime and each row in the sliding window with shape (len(df) - n + 1, n)
+        dt = ((df.index[1:] - df.index[:-1]).days / 365).values
+        dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=0)
+        dt_sliding_window = np.cumsum(dt_sliding_window, axis=0)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=0)
+        dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
+
+        # create sliding window of size n for vix with shape (len(df) - n + 1, n)
+        vix_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 2].values, n)
+
+        data = np.stack((r1_sliding_window, r2_sliding_window, dt_sliding_window, vix_sliding_window), axis=1)
+        self.data = torch.tensor(data, dtype=dtype, requires_grad=False)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        return self.data[idx, :-1, :], self.data[idx, -1, -1]
+
+class VIXDataset2(Dataset):
+    '''
+    Dataset class for VIX data
+    x = (x1, x2) where
+        x1 = (r1_sliding_window, r2_sliding_window, t) each with shape (len(df) - n + 1, n)
+        x2 = vix from 1st time step to 2nd last time step of x1 with shape (len(df) - n + 1, n-1)
+    y = vix level at last time step of x
+    '''
+    def __init__(self, df, n, dtype=torch.float32):
+        '''
+        params:
+        df: pandas.DataFrame
+            dataframe containing the data with columns 'r1', 'r2', 'vix' where 'r1' is the simple return, 'r2' is the squared return and 'vix' is the VIX level to be predicted
+        n: int
+            sliding window size to calculate R1 and R2
+        '''
+        self.len = len(df) - n + 1
+         # create sliding window of size n for r1 and r2 with shape (len(df) - n + 1, n)
+        r1_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 0].values, n)
+        r2_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 1].values, n)
+
+        # calculate time difference between prediction datetime and each row in the sliding window with shape (len(df) - n + 1, n)
+        dt = ((df.index[1:] - df.index[:-1]).days / 365).values
+        dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=0)
+        dt_sliding_window = np.cumsum(dt_sliding_window, axis=0)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=0)
+        dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
+
+        # create sliding window of size n for vix with shape (len(df) - n + 1, n)
+        vix_sliding_window = np.lib.stride_tricks.sliding_window_view(df.iloc[:, 2].values, n)
+
+        data = np.stack((r1_sliding_window, r2_sliding_window, dt_sliding_window, vix_sliding_window), axis=1)
+        self.data = torch.tensor(data, dtype=dtype, requires_grad=False)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        return torch.cat([torch.flatten(self.data[idx, :-1, :]), self.data[idx, -1, :-1]]), self.data[idx, -1, -1]
---
+++ model/train.py
@@ -0,0 +1,257 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class Trainer:
+    '''
+    Trainer class for training a pytorch model
+    params:
+        model: pytorch model
+        optimizer: pytorch optimizer
+        loss_fn: loss function which must take in y and model output as arguments where model output can be a tuple
+        train_dataloader: pytorch dataloader for training data
+        scheduler: pytorch scheduler
+        valid_dataloader: pytorch dataloader for validation data
+        test_dataloader: pytorch dataloader for test data
+        device: device to train on
+        dtype: convert each batch loaded from dataloader to dtype before passing to model
+        path: path to save model weights
+        **kwargs: additional arguments to be passed to loss_fn
+    '''
+    def __init__(self,
+                 model,
+                 optimizer,
+                 loss_fn,
+                 train_dataloader,
+                 scheduler=None,
+                 valid_dataloader=None,
+                 test_dataloader=None,
+                 device='cpu',
+                 dtype=torch.float32,
+                 path='./weights',
+                 **kwargs):
+
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.scheduler = scheduler
+        self.train_dataloader = train_dataloader
+        self.device = device
+        if scheduler is not None:
+            self.scheduler = scheduler
+        if valid_dataloader is not None:
+            self.valid_dataloader = valid_dataloader
+        if test_dataloader is not None:
+            self.test_dataloader = test_dataloader
+        self.dtype = dtype
+        self.path = path
+        self.kwargs = kwargs
+
+        self.batch_log = {}
+        self.epoch_log = {}
+
+    def log_loss(self, key, loss):
+        '''
+        Log loss to self.batch_log
+        '''
+        # if key not in self.log.keys() then create a new key
+        if self.batch_log.get(key) is None:
+            self.batch_log[key] = []
+        else:
+            self.batch_log[key].append(loss)
+
+    def train_epoch_end(self, epoch):
+        '''
+        Function to be called at the end of each epoch
+        '''
+        print(f'Epoch {epoch+1}')
+        print('Training phase:', end=' ')
+        for i, key in enumerate(self.batch_log.keys()):
+            if i > 0:
+                print(' | ', end=' ')
+            if self.epoch_log.get(key) is None:
+                self.epoch_log[key] = []
+            epoch_loss = np.mean(self.batch_log[key])
+            self.epoch_log[key].append(epoch_loss)
+            print(f'{key}: {epoch_loss:.5f}', end=' ')
+        self.batch_log = {}
+        print()
+
+    def valid_epoch_end(self):
+        '''
+        Function to be called at the end of each epoch
+        '''
+        print('Validation phase:', end=' ')
+        for i, key in enumerate(self.batch_log.keys()):
+            if i > 0:
+                print(' | ', end=' ')
+            if self.epoch_log.get(key) is None:
+                self.epoch_log[key] = []
+            epoch_loss = np.mean(self.batch_log[key])
+            self.epoch_log[key].append(epoch_loss)
+            print(f'{key}: {epoch_loss:.5f}', end=' ')
+        self.batch_log = {}
+        print()
+
+    def test_epoch_end(self):
+        '''
+        Function to be called at the end of each epoch
+        '''
+        print('Test phase:', end=' ')
+        for i, key in enumerate(self.batch_log.keys()):
+            if i > 0:
+                print(' | ', end=' ')
+            if self.epoch_log.get(key) is None:
+                self.epoch_log[key] = []
+            epoch_loss = np.mean(self.batch_log[key])
+            self.epoch_log[key].append(epoch_loss)
+            print(f'{key}: {epoch_loss:.5f}', end=' ')
+        self.batch_log = {}
+        print()
+
+    def run_scheduler(self, loss_key):
+        '''
+        Function to run scheduler on loss_key if scheduler is provided
+        '''
+        if hasattr(self, 'scheduler'):
+            if isinstance(self.scheduler, ReduceLROnPlateau):
+                self.scheduler.step(self.epoch_log[loss_key][-1])
+            else:
+                self.scheduler.step()
+
+    def save_model(self, model_name=None):
+        '''
+        Function to save the model to self.path with model_name as the filename
+        '''
+        if model_name is None:
+            model_name = ''
+        else:
+            model_name = '_' + model_name
+        torch.save(self.model.state_dict(), f'{self.path}/model{model_name}.pt')
+
+    def check_best(self, loss_key):
+        '''
+        Function to check if the current epoch is the best epoch based on the loss_key
+        '''
+        if self.epoch_log[loss_key][-1] == min(self.epoch_log[loss_key]):
+            return True
+        else:
+            return False
+
+    def fit(self,
+            n_epochs,
+            model_name=None):
+        '''
+        Function to train the model
+        '''
+        for epoch in range(n_epochs):
+            # Training phase
+            self.model.train()
+            for x, y in self.train_dataloader:
+                x, y = x.to(self.dtype).to(self.device), y.to(self.dtype).to(self.device)
+                self.train_batch(x, y)
+            self.train_epoch_end(epoch)
+
+            # Validation phase if valid_dataloader is provided
+            if hasattr(self, 'valid_dataloader'):
+                self.model.eval()
+                for x, y in self.valid_dataloader:
+                    x, y = x.to(self.dtype).to(self.device), y.to(self.dtype).to(self.device)
+                    self.validate_batch(x, y)
+                self.valid_epoch_end()
+
+                # Save model if it is the best model based on validation loss
+                if model_name is not None and self.check_best('valid_loss'):
+                    print('Saving model')
+                    self.save_model(model_name)
+
+                # Run scheduler on validation loss if provided
+                self.run_scheduler('valid_loss')
+            else:
+                # Save model if it is the best model based on training loss
+                if model_name is not None and self.check_best('train_loss'):
+                    print('Saving model')
+                    self.save_model(model_name)
+
+                # Run scheduler on training loss if provided
+                self.run_scheduler('train_loss')
+
+    def test(self):
+        '''
+        Function to evaluate the model on test data
+        '''
+        if hasattr(self, 'test_dataloader'):
+            self.model.eval()
+            for x, y in self.test_dataloader:
+                x, y = x.to(self.dtype).to(self.device), y.to(self.dtype).to(self.device)
+                self.test_batch(x, y)
+            self.test_epoch_end()
+        else:
+            raise ValueError('Test dataloader not provided')
+
+    def plot_loss(self, loss_keys, figsize=(10, 5)):
+        '''
+        Function to plot the loss for each epoch
+        '''
+        df = pd.DataFrame(self.epoch_log)
+        df[loss_keys].plot(figsize=figsize)
+        plt.legend()
+        plt.show()
+
+class MSETrainer(Trainer):
+    '''
+    Specific train/valid/test steps for AbsValue model
+    '''
+
+    def __init__(self,
+                 model,
+                 optimizer,
+                 loss_fn,
+                 train_dataloader,
+                 scheduler=None,
+                 valid_dataloader=None,
+                 test_dataloader=None,
+                 device='cpu',
+                 dtype=torch.float32,
+                 path='./weights',
+                 **kwargs):
+        super().__init__(model,
+                         optimizer,
+                         loss_fn,
+                         train_dataloader,
+                         scheduler,
+                         valid_dataloader,
+                         test_dataloader,
+                         device,
+                         dtype,
+                         path,
+                         **kwargs)
+
+    def train_batch(self, x, y):
+        '''
+        Function to define a training step
+        '''
+        loss = self.loss_fn(y, self.model(x).squeeze())
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.log_loss('train_loss', loss.item())
+
+    def validate_batch(self, x, y):
+        '''
+        Function to define a validation step
+        '''
+        with torch.no_grad():
+            loss = self.loss_fn(y, self.model(x))
+        self.log_loss('valid_loss', loss.item())
+
+    def test_batch(self, x, y):
+        '''
+        Function to define a test step
+        '''
+        with torch.no_grad():
+            loss = self.loss_fn(y, self.model(x))
+        self.log_loss('test_loss', loss.item())
