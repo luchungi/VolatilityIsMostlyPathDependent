7bb7a6fc1b92fe594887b41abd132e3d475856ce
WSL-Lu <lu.chung.i@gmail.com>
2024-11-09 17:42:36 +0800

    Biz vs cal days

--- model/PDV.py
+++ model/PDV.py
@@ -67,7 +67,7 @@ def R2(λ1, λ2, θ, return_tensor, t):
     '''
     return torch.sqrt(torch.sum(return_tensor * convex_combi_exp_kernel(t, λ1, λ2, θ), dim=-1))
 
-def predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, df, n):
+def predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, df, n, biz_days=False):
     '''
     Predict the VIX level using the PDV model torch version
     params:
@@ -101,35 +101,43 @@ def predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, df, n):
     r2_sliding_window = torch.tensor(np.lib.stride_tricks.sliding_window_view(array[:, 1], n), dtype=torch.float64, requires_grad=False)
 
     # calculate time difference between prediction datetime and each row in the sliding window with shape (len(df) - n + 1, n)
-    dt = ((df.index[1:] - df.index[:-1]).days / 365).values
-    dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
-    dt_sliding_window = np.flip(dt_sliding_window, axis=1)
-    dt_sliding_window = np.cumsum(dt_sliding_window, axis=1)
-    dt_sliding_window = np.flip(dt_sliding_window, axis=1)
-    dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
-    t = torch.tensor(dt_sliding_window, dtype=torch.float64, requires_grad=False)
+    # NOTE: the PDV paper uses business days instead of calendar days but performance is better with calendar days
+    if biz_days:
+        t = torch.ones(len(df) - n + 1, n, dtype=torch.float64, requires_grad=False)
+    else:
+        dt = ((df.index[1:] - df.index[:-1]).days / 365).values
+        dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=1)
+        dt_sliding_window = np.cumsum(dt_sliding_window, axis=1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=1)
+        dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
+        t = torch.tensor(dt_sliding_window, dtype=torch.float64, requires_grad=False)
 
     return β0 + β1 * R1(λ11, λ12, θ1, r1_sliding_window, t) + β2 * R2(λ21, λ22, θ2, r2_sliding_window, t)
 
-def batch_predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, features, datetimeindex, n, return_numpy=False):
+def batch_predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, features, datetimeindex, n, return_numpy=False, biz_days=False):
     # create sliding window of size n for r1 and r2 with shape (len(df) - n + 1, n)
     r1_sliding_window = torch.tensor(np.lib.stride_tricks.sliding_window_view(features[:, :, 0], n, axis=-1), dtype=torch.float64, requires_grad=False)
     r2_sliding_window = torch.tensor(np.lib.stride_tricks.sliding_window_view(features[:, :, 1], n, axis=-1), dtype=torch.float64, requires_grad=False)
 
     # calculate time difference between prediction datetime and each row in the sliding window with shape (len(df) - n + 1, n)
-    dt = ((datetimeindex[1:] - datetimeindex[:-1]).days / 365).values
-    dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
-    dt_sliding_window = np.flip(dt_sliding_window, axis=1)
-    dt_sliding_window = np.cumsum(dt_sliding_window, axis=1)
-    dt_sliding_window = np.flip(dt_sliding_window, axis=1)
-    dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
-    t = torch.tensor(dt_sliding_window, dtype=torch.float64, requires_grad=False).unsqueeze(0)
+    # NOTE: the PDV paper uses business days instead of calendar days but performance is better with calendar days
+    if biz_days:
+        t = torch.ones(len(datetimeindex) - n + 1, n, dtype=torch.float64, requires_grad=False)
+    else:
+        dt = ((datetimeindex[1:] - datetimeindex[:-1]).days / 365).values
+        dt_sliding_window = np.lib.stride_tricks.sliding_window_view(dt, n-1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=1)
+        dt_sliding_window = np.cumsum(dt_sliding_window, axis=1)
+        dt_sliding_window = np.flip(dt_sliding_window, axis=1)
+        dt_sliding_window = np.concatenate((dt_sliding_window, np.zeros((dt_sliding_window.shape[0], 1))), axis=1)
+        t = torch.tensor(dt_sliding_window, dtype=torch.float64, requires_grad=False).unsqueeze(0)
 
     preds = β0 + β1 * R1(λ11, λ12, θ1, r1_sliding_window, t) + β2 * R2(λ21, λ22, θ2, r2_sliding_window, t)
 
     return preds.numpy() if return_numpy else preds
 
-def residual(x, df, n, return_y_hat=False):
+def residual(x, df, n, return_y_hat=False, biz_days=False):
     '''
     Calculate the residuals of the PDV model torch version
     params:
@@ -142,7 +150,7 @@ def residual(x, df, n, return_y_hat=False):
     '''
     β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2 = x
     y = df.iloc[n-1:]['vix'].values # shape (len(df) - n + 1,) = number of predictions
-    y_hat = predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, df, n)
+    y_hat = predict(β0, β1, β2, λ11, λ12, θ1, λ21, λ22, θ2, df, n, biz_days)
     if return_y_hat:
         return y - y_hat.numpy(), y_hat.numpy()
     else:
@@ -155,7 +163,7 @@ def torch_predict(params, x):
     t = x[:, 2, :]
     return β0 + β1 * R1(λ11, λ12, θ1, r1_sliding_window, t) + β2 * R2(λ21, λ22, θ2, r2_sliding_window, t)
 
-def evaluate(params, data, window):
+def evaluate(params, data, window, biz_days=False):
     '''
     Evaluate the PDV model calculated using the sliding window of size window
     Prints the mean, min, max, mean absolute error, mean squared error and R^2 of the residuals
@@ -167,12 +175,12 @@ def evaluate(params, data, window):
     window: int
         sliding window size to calculate R1 and R2
     '''
-    residuals = residual(params, data, window)
+    residuals = residual(params, data, window, biz_days=biz_days)
     total_sum_of_squares = np.sum((data.iloc[window-1:]['vix'] - np.mean(data.iloc[window-1:]['vix'])) ** 2)
     print('Mean: {:.4f}, Min: {:.4f}, Max: {:.4f}, MAE: {:.4f}, MSE: {:.4f}, R^2: {:.4f}'.format(
         np.mean(residuals), np.min(residuals), np.max(residuals), np.mean(np.abs(residuals)), np.mean(residuals**2), (1-np.sum(residuals**2)/total_sum_of_squares)))
 
-def plot(params, data, window, valid_start_date=None, test_start_date=None):
+def plot(params, data, window, valid_start_date=None, test_start_date=None, biz_days=False):
     '''
     Plot the VIX level and the predicted VIX level using the PDV model calculated using the sliding window of size window
     params:
@@ -187,7 +195,7 @@ def plot(params, data, window, valid_start_date=None, test_start_date=None):
     test_start_date: datetime.date
         start date of the test period
     '''
-    preds = predict(*params, data, window)
+    preds = predict(*params, data, window, biz_days)
     df_preds = data[window-1:].copy()
     df_preds['preds'] = preds
     df_preds[['vix', 'preds']].plot(figsize=(15, 10))
